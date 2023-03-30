c63e57cd2ebb6dd4d0c84eb7bc1ebd07532fc021
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-03-30 11:56:10 -0700

    feat: move to app/ directory from pages/

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import Sidebar from "../components/sidebar";
+import "./globals.css";
+
+export const metadata = {
+  title: "Rey Barceló",
+  description: "My portfolio site",
+};
+
+export default function RootLayout({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="bg-black text-white">
+      <body>
+        {" "}
+        <Sidebar />
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
