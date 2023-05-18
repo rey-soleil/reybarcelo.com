25658fbdeff1453027b130f3f8edce6ce7fa6e4f
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-05-18 10:56:51 -0700

    feat(writing): display list of posts

--- app/writing/page.tsx
+++ app/writing/page.tsx
@@ -1,7 +1,22 @@
+import { getSortedPostsData } from "../../lib/writing";
+
 export default function Writing() {
+  const allPostsData = getSortedPostsData();
+
   return (
     <>
       <h1 className="header">Writing</h1>
+      <div className="paragraph">
+        {/* TODO(rey-soleil): add style */}
+        <ul>
+          {allPostsData.map(({ id, date, title }) => (
+            <li key={title}>
+              {title}
+              {date}
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
